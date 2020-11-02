a991499b04328623c557e43b73e2216f704b586e
rekebas <aekebas@hotmail.com>
2020-11-01 22:23:47 -0600

    second commit

--- src/Main/Workout.java
+++ src/Main/Workout.java
@@ -5,11 +5,7 @@ public class Workout {
     public static void main(String[] args) {
 
         System.out.println(StrReverse("dd"));
-        System.out.println(StrReverse("Ekddebas"));
-        System.out.println(StrReverse("Ekeddbas"));
-        System.out.println(StrReverse("Richard"));
-        System.out.println(StrReverse("Ekebas"));
-        System.out.println(StrReverse("Ekebas"));
+
 
 
 
